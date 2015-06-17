8764dfc56938b7e1efee2c9af386542a163ab393
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-06-16 20:05:01 -0700

    Version one of the program is complete.
    Now comes testing.

--- Main.cs
+++ Main.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace SetAffinity
 {
@@ -36,7 +37,12 @@ namespace SetAffinity
 		subprogramArguments.Clear();
 		// divide up the number of arguments between sub and this program
 		subprogramArguments.AddRange(args.Skip(args.IndexOf("--") + 1));
-		return ParseProgramArguments(args.Take(args.IndexOf("--")));
+		if (subprogramArguments.Count() == 0) {
+		    Console.WriteLine("No program specified on command line!");
+		    return false;
+		} else {
+		    return ParseProgramArguments(args.Take(args.IndexOf("--")));
+		}
 	    }
 	}
 	private static bool ParseProgramArguments(IEnumerable<string> args)
@@ -48,25 +54,71 @@ namespace SetAffinity
 		Console.WriteLine("Too many arguments provided! Affinity mask only");
 		return false;
 	    } else {
-		if (!int.TryParse(args.First(), out affinity)) {
-		    Console.WriteLine("Couldn't parse affinity mask!");
-		    return false;
-		} else {
+		try {
+		    affinity = int.Parse(args.First(), NumberStyles.HexNumber, null);
 		    return true;
+		} catch(FormatException) {
+		    try {
+			affinity = int.Parse(args.First());
+			return true;
+		    } catch (Exception) {
+			Console.WriteLine("Couldn't parse affinity mask! {0}", args.First());
+			return false;
+		    }
 		}
+
 	    }
 	}
-	public static class Program
+	public static string Join(IEnumerable<string> contents, string delimiter)
 	{
-	    public static void Main(string[] args)
-	    {
-		if (!Args.Parse(args)) {
+	    StringBuilder sb = new StringBuilder();
+	    foreach (var v in contents) {
+		sb.Append(v);
+		sb.Append(delimiter);
+	    }
+	    return sb.ToString();
+	}
+	public static string Join(IEnumerable<string> contents)
+	{
+	    return Join(contents, " ");
+	}
+	public static ProcessStartInfo GetProcess()
+	{
+	    if (subprogramArguments.Count() == 0) {
+		Console.WriteLine("No subprogram specified!");
+		return null;
+	    } else {
+		return new ProcessStartInfo(subprogramArguments.First(),
+			Join((from x in subprogramArguments.Skip(1)
+				select string.Format("\"{0}\"", x))));
+	    }
+	}
+    }
+    public static class Program
+    {
+	public static void Usage()
+	{
+	    Console.WriteLine("Usage: set-affinity.exe <affinity-mask> -- <program> <program-args>");
+	}
+	public static void Main(string[] args)
+	{
+	    if (!Args.Parse(args)) {
+		Usage();
+		return;
+	    }
+	    ProcessStartInfo psi = Args.GetProcess();
+	    if (psi == null) {
+		Console.WriteLine("No program to execute specified!");
+		return;
+	    } else {
+		Process p = Process.Start(psi);
+		if (p == null) {
+		    Console.WriteLine("Couldn't start {0}", psi.FileName);
 		    return;
-		}
-		Console.WriteLine("Affinity mask: {0}", Args.Affinity);
-		Console.WriteLine("--");
-		foreach (var v in Args.SubprogramArguments) {
-		    Console.WriteLine(v);
+		} else {
+		    Console.WriteLine("Setting affinity for process {0} to {1}", psi.FileName, Args.Affinity);
+		    p.ProcessorAffinity = (IntPtr)Args.Affinity;
+		    Console.WriteLine("Bye!");
 		}
 	    }
 	}
